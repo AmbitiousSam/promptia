c73ecb6e359d927f4a521e99ff02b55408f7ef80
KVS SIVA KUMAR <sivakumar.k@comakeit.com>
2023-06-18 11:50:06 +0530

    Typo Error Fix in Create-Prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -3,79 +3,45 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
   const router = useRouter();
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
-  if (status === "loading") {
-    return <div className="gradient_orange font-satoshi font-semibold text-base">Loading...</div>;
-  }
-
-  if (status === "error") {
-    return <div className="gradient_orange font-satoshi font-semibold text-base">Error: {session.error.message}</div>;
-  }
-
-  if (status === "not-found") {
-    return <div className="gradient_orange font-satoshi font-semibold text-base">Not found</div>;
-  }
-
-  if (status === "ok") {
-    return (
-      <div>
-        <h1>Create Prompt</h1>
-        <Form
-          onSubmit={async (values) => {
-            setSubmitting(true);
-            await router.push("/");
-            setSubmitting(false);
-          }}
-        />
-      </div>
-    );
-  }
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
+
     try {
-      const res = await fetch("/api/prompt/new", {
+      const response = await fetch("/api/prompt/new", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
           tag: post.tag,
         }),
       });
-      const data = await res.json();
-      if (res.ok) {
-        setPost({
-          prompt: "",
-          tag: "",
-        });
-        setSubmitting(false);
+
+      if (response.ok) {
         router.push("/");
       }
-    } catch (err) {
-      console.log(err);
-      setSubmitting(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
     <Form
-      type="create"
+      type='Create'
       post={post}
-      setpost={setPost}
+      setPost={setPost}
       submitting={submitting}
       handleSubmit={createPrompt}
     />
